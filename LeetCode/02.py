50b2c88a996c601dc17461f29201b7c9998c5de9
Sagar Paul <paul.samir.2002@gmail.com>
2021-05-24 00:55:57 +0530

    valid palindrom

--- LeetCode/02.py
+++ LeetCode/02.py
@@ -1,10 +1,25 @@
-nums = [-1,0,0,0,0,3,3]
-i = 0
-while i< len(nums)-1:
-    if nums[i] == nums[i+1]:
-        nums.pop(nums[i])
-    else:
-        i +=1
+class Solution:
+    def moveZeroes(self, nums):
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+        # After this loop, nums = [1,3,12,3,12]
+        
+        if n >1:
+            for i in range(n-count,n):
+                nums[i] = 0
 
+            #print(nums)
+        
+        # After this loop, nums = [1,3,12,0,0]
 
-print(len(nums))
+        
+        return nums
+
+nums = [0,1,0,3,12]
+
+ll = Solution()
+print(ll.moveZeroes(nums))
